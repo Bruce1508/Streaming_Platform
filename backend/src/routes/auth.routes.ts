a6ad9c71b91f20538e1e801af264bb7bb3731aac
Bruce1508 <brucevo158@gmail.com>
2025-05-15 15:14:25 -0400

    add authentication steps

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request,Response} from "express";
 import { signIn, signOut, signUp, onBoarding } from "../controllers/auth.controllers";
 import { protectRoute } from "../middleWare/auth.middleware";
 
@@ -11,6 +11,12 @@ router.post("/sign-up", signUp);
 router.post("/sign-out", signOut);
 
 router.post("/onboarding",protectRoute, onBoarding);
+
+// This route is protected and requires authentication
+// The protectRoute middleware checks if the user is authenticated
+router.get("/me", protectRoute, (req:Request, res:Response) => {
+    res.status(200).json({success:true, user:req.user});
+});
 
 export default router;
 // This code defines a set of routes for user authentication in an Express application.
